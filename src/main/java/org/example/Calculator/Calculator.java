bbb5e95087fb5003863d8f314fa131a1e1f8adaa
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-03-06 13:53:06 +0100

    feat second test substract

--- src/main/java/org/example/Calculator/Calculator.java
+++ src/main/java/org/example/Calculator/Calculator.java
@@ -1,5 +1,7 @@
 package org.example.Calculator;
 
 public class Calculator {
-
+    public static float sum (float numberOne, float numberTwo){
+        return numberOne + numberTwo ;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,14 +5,25 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CalculatorTest {
     @ParameterizedTest
     @CsvSource  ({
-            "3, 3",
+            "1,2, 3",
+            "4,5, 9"
     })
-    void check_sum_numberOne_plus_numberTwo(int numberOne, int numberTwo, int expectedOutput){
-        assertEquals(expectedOutput, new Calculator().sum(int numberOne + int numberTwo));
+    void check_sum_numberOne_plus_numberTwo(float numberOne, float numberTwo, float expectedOutput){
+        assertEquals(expectedOutput, Calculator.sum(numberOne, numberTwo));
+        assertNotEquals(4,Calculator.sum(numberOne, numberTwo));
+    }
+    @ParameterizedTest
+    @CsvSource ({
+            "1,2, -1",
+            "4,5, -1",
+    })
+    void check_substract_numberOne_numberTwo(float numberOne, float numberTwo, float expectedOutput){
+        assertEquals(expectedOutput, Calculator.substrac(numberOne,numberTwo));
     }
 }
 
